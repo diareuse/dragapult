cd75a0444fe63b32f23f8231675af9341aa6a065
Viktor De Pasquale <hello@depasquale.wiki>
2025-04-04 20:52:35 +0200

    feat(reader): add YAML support for translation I/O
    - Introduced `ReaderYamlIR` for reading translations from YAML.
    - Added `WriterYamlIR` for writing translations to YAML.
    - Provided tests to verify compatibility between JSON and YAML IRs.

---
+++ dragapult-app/src/main/java/dragapult/app/v2/reader/ReaderYamlIR.kt
@@ -0,0 +1,73 @@
+package dragapult.app.v2.reader
+
+import com.charleskorn.kaml.Yaml
+import com.charleskorn.kaml.YamlConfiguration
+import com.charleskorn.kaml.decodeFromStream
+import dragapult.app.v2.TranslationKeyIR
+import dragapult.app.v2.TranslationReader
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.io.InputStream
+import java.util.*
+
+class ReaderYamlIR(
+    private val input: InputStream
+) : TranslationReader {
+    @OptIn(ExperimentalSerializationApi::class)
+    private val yaml = Yaml(
+        configuration = YamlConfiguration(
+            encodeDefaults = false,
+            strictMode = false,
+            codePointLimit = Int.MAX_VALUE
+        )
+    )
+
+    val map = yaml.decodeFromStream<List<Key>>(input)
+    val iter = map.iterator()
+
+    override fun hasNext(): Boolean {
+        return iter.hasNext()
+    }
+
+    override fun next(): TranslationKeyIR {
+        return TranslationKeyIRFromKey(iter.next())
+    }
+
+    @JvmInline
+    private value class TranslationKeyIRFromKey(private val entry: Key) : TranslationKeyIR {
+        override val key: String
+            get() = entry.name
+        override val metadata: TranslationKeyIR.Metadata?
+            get() = object : TranslationKeyIR.Metadata {
+                override val comment: String?
+                    get() = entry.comment
+                override val properties: Map<String, String>?
+                    get() = entry.properties?.associate { it.name to it.value }
+            }.takeIf { it.comment != null || it.properties != null }
+        override val translations: Map<Locale, String>
+            get() = entry.translations.mapKeys { Locale.forLanguageTag(it.key) }
+    }
+
+
+    @Serializable
+    data class Key(
+        @SerialName("key")
+        val name: String,
+        @SerialName("comment")
+        val comment: String? = null,
+        @SerialName("properties")
+        val properties: List<Property>? = null,
+        @SerialName("translations")
+        val translations: Map<String, String>
+    )
+
+    @Serializable
+    data class Property(
+        @SerialName("name")
+        val name: String,
+        @SerialName("value")
+        val value: String
+    )
+
+}
---
+++ dragapult-app/src/test/java/dragapult/app/YamlIRTest.kt
@@ -0,0 +1,53 @@
+package dragapult.app
+
+import dragapult.app.v2.reader.ReaderJsonIR
+import dragapult.app.v2.reader.ReaderYamlIR
+import dragapult.app.v2.reader.WriterJsonIR
+import dragapult.app.v2.reader.WriterYamlIR
+import java.io.ByteArrayOutputStream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class YamlIRTest : ConversionHarness() {
+
+    @Test
+    fun `read from yaml IR matches json IR`() = test(
+        prepare = {
+            val output = ByteArrayOutputStream()
+            val reader = ReaderYamlIR(resourceFile("ir/keys.yaml.ir"))
+            val writer = WriterJsonIR(output)
+            TestSetup.ToIR(reader, writer, output)
+        },
+        test = { (reader, writer, output) ->
+            reader.copyTo(writer)
+            output.toString("UTF-8")
+        },
+        verify = { actual ->
+            assertEquals(
+                expected = resourceFileAsString("ir/keys.json.ir"),
+                actual = actual
+            )
+        }
+    )
+
+    @Test
+    fun `read from json IR matches yaml IR`() = test(
+        prepare = {
+            val reader = ReaderJsonIR(resourceFile("ir/keys.json.ir"))
+            val output = ByteArrayOutputStream()
+            val writer = WriterYamlIR(output)
+            TestSetup.ToIR(reader, writer, output)
+        },
+        test = { (reader, writer, output) ->
+            reader.copyTo(writer)
+            output.toString("UTF-8")
+        },
+        verify = { actual ->
+            assertEquals(
+                expected = resourceFileAsString("ir/keys.yaml.ir"),
+                actual = actual
+            )
+        }
+    )
+
+}
