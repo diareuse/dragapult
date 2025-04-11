3506d738ba177857b79a52875008d1d1fbb77bcb
Viktor De Pasquale <hello@depasquale.wiki>
2025-04-11 18:45:09 +0200

    refactor: refactor TranslationPlugin to allow inline functional typing

--- dragapult-app/src/main/java/dragapult/app/TranslationPlugin.kt
+++ dragapult-app/src/main/java/dragapult/app/TranslationPlugin.kt
@@ -1,6 +1,25 @@
 package dragapult.app
 
 interface TranslationPlugin {
+
     val priority: Int
     fun modify(key: TranslationKeyIR): TranslationKeyIR
+
+    companion object {
+        operator fun invoke(
+            priority: Int = Int.MAX_VALUE / 2,
+            body: (TranslationKeyIR) -> TranslationKeyIR
+        ): TranslationPlugin = InlineTranslationPlugin(
+            priority = priority,
+            body = body
+        )
+    }
+
+}
+
+private class InlineTranslationPlugin(
+    override val priority: Int,
+    private val body: (TranslationKeyIR) -> TranslationKeyIR
+) : TranslationPlugin {
+    override fun modify(key: TranslationKeyIR) = body(key)
 }
--- dragapult-app/src/main/java/dragapult/app/di/PluginModule.kt
+++ dragapult-app/src/main/java/dragapult/app/di/PluginModule.kt
@@ -3,7 +3,6 @@ package dragapult.app.di
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoSet
-import dragapult.app.TranslationKeyIR
 import dragapult.app.TranslationPlugin
 
 @Module
@@ -11,9 +10,6 @@ class PluginModule {
 
     @IntoSet
     @Provides
-    fun noop() = object : TranslationPlugin {
-        override val priority: Int get() = Int.MAX_VALUE
-        override fun modify(key: TranslationKeyIR) = key
-    }
+    fun noop() = TranslationPlugin(Int.MAX_VALUE) { it }
 
 }
